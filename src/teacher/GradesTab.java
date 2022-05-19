dfe5c2239d8c754ea986582c391f9c0e3637fe6f
Dredhza Braina <94399125+DredhzaBraina@users.noreply.github.com>
2022-05-19 21:02:23 +0200

    Update GradesTab.java

--- src/teacher/GradesTab.java
+++ src/teacher/GradesTab.java
@@ -1,1 +1,28 @@
+package teacher;
+
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import menu.I18N;
+
+public class GradesTab {
+    Tab tab;
+
+    public GradesTab(Tab tab) {
+        this.tab = tab;
+    }
 
