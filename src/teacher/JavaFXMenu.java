0021f16380cc1d4feab95b256fd61435d41f4d4e
artukshini <94797611+artukshini@users.noreply.github.com>
2022-05-21 00:39:58 +0200

    Update JavaFXMenu.java

--- src/teacher/JavaFXMenu.java
+++ src/teacher/JavaFXMenu.java
@@ -1,1 +1,29 @@
+package teacher;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import menu.MainMenu;
+
+public class JavaFXMenu
+{
+	private BorderPane root = new BorderPane();
+	private TabPane tabPane = new TabPane();
+	List<Tab> openTabs = new ArrayList<>();
+	SingleSelectionModel<Tab> selectionModel = tabPane.getSelectionModel();
