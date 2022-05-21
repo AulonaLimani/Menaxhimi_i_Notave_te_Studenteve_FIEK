7829b61144d02bec832f1293b6182977392530cb
Dredhza Braina <94399125+DredhzaBraina@users.noreply.github.com>
2022-05-21 18:12:44 +0200

    Update TeachersPanel.java

--- src/admin/TeachersPanel.java
+++ src/admin/TeachersPanel.java
@@ -50,3 +50,56 @@ public class TeachersPanel {
 	private TextField addressID = new TextField();
 	private TextField subjectID = new TextField();
 	private TableView teachersTable = new TableView();
+	
+	public void createStage() {
+
+		About obj = new About();
+
+		btnSave = I18N.getButton("save");
+		Stage stage = new Stage();
+
+		BorderPane pane = new BorderPane();
+		VBox vboxMain = new VBox();
+
+		BorderPane root = new BorderPane();
+
+		MenuBar menuBar = new MenuBar();
+
+		Menu file = I18N.getMenu("file");
+
+		MenuItem itmClose = I18N.getMenuItem("close");
+		itmClose.setOnAction(e -> {
+			stage.hide();
+		});
+
+		Menu edit = I18N.getMenu("editMenu");
+		Menu language = I18N.getMenu("language");
+
+		MenuItem itmShqip = I18N.getMenuItem("shqip");
+		MenuItem itmEnglish = I18N.getMenuItem("english");
+		language.getItems().addAll(itmShqip, itmEnglish);
+		edit.getItems().add(language);
+		itmShqip.setOnAction(e -> {
+			I18N.setLocale(new Locale("al"));
+		});
+		itmEnglish.setOnAction(e -> {
+			I18N.setLocale(new Locale("en"));
+		});
+
+		MenuItem itmStudents = I18N.getMenuItem("studentsPanel");
+		itmStudents.setOnAction(e -> {
+			new StudentsPanel().createStage();
+		});
+
+		Menu help = I18N.getMenu("help");
+
+		MenuItem itmAbout = I18N.getMenuItem("about");
+		MenuItem itmHelp = I18N.getMenuItem("help");
+		itmHelp.setOnAction(e -> {
+			if(itmHelp.getText().equals("Help"))
+			{
+				HelpAdminEn.about();
+			}else {
+				AdminHelp.about();
+			}
+		});
