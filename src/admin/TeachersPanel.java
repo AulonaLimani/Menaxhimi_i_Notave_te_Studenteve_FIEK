b7a1b035e695ebe8fab058789c04b43fd1cdf01d
Dredhza Braina <94399125+DredhzaBraina@users.noreply.github.com>
2022-05-21 18:11:37 +0200

    Update TeachersPanel.java

--- src/admin/TeachersPanel.java
+++ src/admin/TeachersPanel.java
@@ -1,1 +1,31 @@
+package admin;
 
+import java.util.List;
+import java.util.Locale;
+
+import Help.About;
+import Help.AdminHelp;
+import Help.HelpAdminEn;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import menu.I18N;
