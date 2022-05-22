adeccd730b6c934c86b534fc610e8f9330aae6d1
artukshini <94797611+artukshini@users.noreply.github.com>
2022-05-22 18:33:28 +0200

    Update MainMenu.java

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -1,7 +1,13 @@
 package menu;
 
-public class MainMenu
-{
+import java.util.Locale;
 
-
-}
+import Help.About;
+import Help.AdminHelp;
+import Help.HelpAdminEn;
+import Help.HelpStudentEn;
+import Help.HelpTeacher;
+import Help.teacherHelp;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
